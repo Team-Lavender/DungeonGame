e2d6926e56e08ea92d7d58562445bc5ac923692f
dpc44 <72976497+dpc44@users.noreply.github.com>
2020-11-01 21:06:50 +0000

    Create actor.py
    class actor

---
+++ actor.py
@@ -0,0 +1,8 @@
+class Actor:
+    def __init__(self, pos_x, pos_y, sprite):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.sprite = sprite
+
+    def render(self):
+        pass
