a9e3a17ace30e4839dfa21ae3dedd703ba2477c0
matt-horder <matt.horder@outlook.com>
2020-11-02 19:06:21 +0000

    added control, state, and menu_manager modules as per pygame state_machine

---
+++ control.py
@@ -0,0 +1,22 @@
+
+class Control:
+    def __init__(self):
+        pass
+
+    def setup_states(self):
+        pass
+
+    def flip_state(self):
+        pass
+
+    def update(self):
+        pass
+
+    def event_loop(self):
+        pass
+
+    def main_game_loop(self):
+        pass
+
+
+
---
+++ menu_manager.py
@@ -0,0 +1,13 @@
+
+class MenuManager:
+    def __init__(self):
+        pass
+
+    def draw_menu(self):
+        pass
+
+    def update_menu(self):
+        pass
+
+    def get_event_menu(self):
+        pass
