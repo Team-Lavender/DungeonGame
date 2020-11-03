6715ef3f4af17961273a8ea2440ca57220fd1283
matt-horder <matt.horder@outlook.com>
2020-11-03 13:03:01 +0000

    added .gitignore created skeletons for control, menu_manager, and state

---
+++ control.py
@@ -0,0 +1,20 @@
+# State machine: https://python-forum.io/Thread-PyGame-Creating-a-state-machine
+
+class Control:
+    def __init__(self):
+        pass
+
+    def setup_states(self):
+        pass
+
+    def flip_state(self):
+        pass
+
+    def update(self):
+        pass
+
+    def event_loop(self):
+        pass
+
+    def main_game_loop(self):
+        pass
