192e77ac4471b46accb8cfde3ca9b634d5f10ef3
WelchGeorge <georgewelch@me.com>
2020-11-04 17:26:50 +0000

    filled in use_item and swap_item methods for player. added blast effect animation

--- player.py
+++ player.py
@@ -1,7 +1,8 @@
-import pygame
 
+import config
 from entities import *
 
+from weapon import*
 
 class Player(Entity):
     def __init__(self, game, pos_x, pos_y, sprite, health, shield, has_ai, entity_level, entity_status, move_speed,
@@ -9,14 +10,31 @@ class Player(Entity):
         super(Player, self).__init__(game, pos_x, pos_y, sprite, health, shield, has_ai, entity_level, entity_status,
                                      move_speed)
         self.money = money
-        self.held_item = None
-        self.items = []
+
+        self.items = \
+            [Weapon(game, self.pos_x, self.pos_y, config.get_weapon_sprite("knight_sword"), 1,1, "melee", 1, 1, 2, 5),
+             Weapon(game, self.pos_x, self.pos_y, config.get_weapon_sprite("rusty_sword"), 1,1, "melee", 1, 1, 1, 5),
+             Weapon(game, self.pos_x, self.pos_y, config.get_weapon_sprite("bow"), 1,1, "melee", 1, 1, 0.5, 5)]
+        self.held_item = self.items[0]
+        self.held_item.in_inventory = False
+        self.look_direction = pygame.Vector2(1, 0)
 
     def use_item(self):
-        pass
+        if isinstance(self.held_item, Weapon) and\
+                pygame.time.get_ticks() - self.held_item.last_used >= 1000 * self.held_item.attack_speed:
+            self.held_item.state = "blast"
+            self.held_item.last_used = pygame.time.get_ticks()
+            self.attack()
+        else:
+            pass
 
-    def swap_item(self):
-        pass
+    def swap_item(self, next_or_prev):
+        if len(self.items) > 0:
+            self.held_item.in_inventory = True
+            self.held_item = self.items[(self.items.index(self.held_item) + next_or_prev) % len(self.items)]
+            self.held_item.in_inventory = False
+        else:
+            pass
 
     def get_input(self):
         dx = 0
@@ -35,6 +53,24 @@ class Player(Entity):
             direction.scale_to_length(self.move_speed)
         self.move(direction)
         dx, dy = 0, 0
+
+        mouse_vector = pygame.mouse.get_pos()
+        look_vector = pygame.Vector2((mouse_vector[0] - self.pos_x), (mouse_vector[1] - self.pos_y))
+        self.look_direction = look_vector.normalize()
+        if isinstance(self.held_item, Weapon):
+            self.held_item.pos_x = self.pos_x
+            self.held_item.pos_y = self.pos_y
+            self.held_item.target_direction = self.look_direction
+            self.held_item.angle = self.look_direction.angle_to(pygame.Vector2(0, -1))
+
+        if self.game.ACTION:
+            self.use_item()
+
+        if self.game.SCROLL_UP:
+            self.swap_item(1)
+
+        if self.game.SCROLL_DOWN:
+            self.swap_item(-1)
 
     def mine(self):
         pass
--- weapon.py
+++ weapon.py
@@ -1,4 +1,5 @@
 from item import *
+import pygame
 
 
 class Weapon(Item):
@@ -9,3 +10,29 @@ class Weapon(Item):
         self.attack_damage = dmg
         self.attack_speed = speed
         self.crit_chance = crit_chance
+        self.target_direction = pygame.Vector2(1, 0)
+        self.angle = self.target_direction.angle_to(pygame.Vector2(0, -1))
+        self.in_inventory = True
+        self.last_used = pygame.time.get_ticks()
+
+    def render(self):
+        if pygame.time.get_ticks() - self.last_used >= 400:
+            self.state = "idle"
+        offset = self.target_direction
+        offset.scale_to_length(2)
+        frame_set = self.sprite[self.state]
+        anim_length = len(frame_set)
+        self.frame %= anim_length
+        curr_frame = frame_set[self.frame]
+        frame_rect = curr_frame.get_rect()
+        frame_rect.center = (self.pos_x, self.pos_y - 10)
+        center = ((frame_rect.centerx + offset[0] * frame_rect[3] / 2), (frame_rect.centery + offset[1] * frame_rect[3] / 2))
+        if self.update_frame == 0:
+            self.frame = (self.frame + 1) % anim_length
+        self.update_frame = (self.update_frame + 1) % 6
+        curr_frame = pygame.transform.rotate(curr_frame, self.angle)
+        new_rect = curr_frame.get_rect()
+        new_rect.center = center
+
+        if config.is_in_window(center[0], center[1]) and not self.in_inventory:
+            self.game.display.blit(curr_frame, new_rect)
