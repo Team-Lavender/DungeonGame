dd8b839e8aa2ffa0646af875d0690e7bb121ad09
WelchGeorge <georgewelch@me.com>
2020-11-06 17:25:25 +0000

    added character classes and starting equipment

---
+++ character_classes.py
@@ -0,0 +1,49 @@
+# character stats start from 0, with 10 being an average stat
+
+character_stats = {"PALADIN": {"str": 16,
+                               "dex": 9,
+                               "con": 16,
+                               "int": 8,
+                               "wis": 8,
+                               "cha": 10},
+
+                   "RANGER": {"str": 12,
+                              "dex": 16,
+                              "con": 12,
+                              "int": 10,
+                              "wis": 10,
+                              "cha": 10},
+
+                   "MAGE": {"str": 8,
+                            "dex": 10,
+                            "con": 8,
+                            "int": 16,
+                            "wis": 14,
+                            "cha": 12},
+
+                   "ROGUE": {"str": 10,
+                             "dex": 16,
+                             "con": 10,
+                             "int": 12,
+                             "wis": 14,
+                             "cha": 14}}
+
+starting_equipment = {"PALADIN": {"weapon": "knight_sword",
+                                  "armor": "chainmail",
+                                  "potion": None,
+                                  "throwable": None},
+
+                      "RANGER": {"weapon": "bow",
+                                 "armor": "leather",
+                                 "potion": None,
+                                 "throwable": None},
+
+                      "MAGE": {"weapon": "green_magic_staff",
+                               "armor": "none",
+                               "potion": None,
+                               "throwable": None},
+
+                      "ROGUE": {"weapon": "knife",
+                                "armor": "none",
+                                "potion": None,
+                                "throwable": None}}
