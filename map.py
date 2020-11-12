34ef4007969cb5e7868db9b956ac6b39d3d67ad0
Hsuanyin <hsuanyin81@gmail.com>
2020-11-12 22:22:27 +0000

    fix draw_map and render_map

--- map.py
+++ map.py
@@ -6,6 +6,7 @@ import configparser
 class Map:
     def __init__(self, game, height, width):
         self.game = game
+        self.render_space = 0.7
         self.tool_bar = 5
         self.map_set = {}
         self.height = height
@@ -42,41 +43,68 @@ class Map:
                     self.unpassable.add((x, y + self.tool_bar))
 
     def draw_map(self):
-
+        # extract wall tiles
         wall_mid = self.get_tiles(self.parser.get("tilesets", "wall_mid"))
-        # wall_side_front_left = self.get_tiles(self.parser.get("tilesets", "wall_side_front_left"))
-        # wall_side_front_right = self.get_tiles(self.parser.get("tilesets", "wall_side_front_right"))
+        wall_left = self.get_tiles(self.parser.get("tilesets", "wall_left"))
+        wall_right = self.get_tiles(self.parser.get("tilesets", "wall_right"))
+        wall_top_mid = self.get_tiles(self.parser.get("tilesets", "wall_top_mid"))
+        wall_top_left = self.get_tiles(self.parser.get("tilesets", "wall_top_left"))
+        wall_top_right = self.get_tiles(self.parser.get("tilesets", "wall_top_right"))
         wall_side_mid_left = self.get_tiles(self.parser.get("tilesets", "wall_side_mid_left"))
         wall_side_mid_right = self.get_tiles(self.parser.get("tilesets", "wall_side_mid_right"))
         wall_inner_corner_mid_left = self.get_tiles(self.parser.get("tilesets", "wall_inner_corner_mid_left"))
-        wall_top_left = self.get_tiles(self.parser.get("tilesets", "wall_top_left"))
-        wall_inner_corner_mid_rigth = self.get_tiles(self.parser.get("tilesets", "wall_inner_corner_mid_rigth"))
-        wall_top_right = self.get_tiles(self.parser.get("tilesets", "wall_top_right"))
-        wall_top_mid = self.get_tiles(self.parser.get("tilesets", "wall_top_mid"))
+        wall_inner_corner_mid_right = self.get_tiles(self.parser.get("tilesets", "wall_inner_corner_mid_right"))
+        wall_corner_top_left = self.get_tiles((self.parser.get("tilesets", "wall_corner_top_left")))
+        wall_corner_top_right = self.get_tiles((self.parser.get("tilesets", "wall_corner_top_right")))
+        wall_side_front_left = self.get_tiles(self.parser.get("tilesets", "wall_side_front_left"))
+        wall_side_front_right = self.get_tiles(self.parser.get("tilesets", "wall_side_front_right"))
+        wall_side_top_left = self.get_tiles(self.parser.get("tilesets", "wall_side_top_left"))
+        wall_side_top_right = self.get_tiles(self.parser.get("tilesets", "wall_side_top_right"))
+
+
+        # extract other object tiles
         plant = self.get_tiles(self.parser.get("tilesets", "plant"))
         chest = self.get_tiles(self.parser.get("tilesets", "object"))
         floor = self.get_tiles(self.parser.get("tilesets", "floor"))
+        # draw walls
         for x, y in self.wall:
-
             value = self.wall_render(x, y)
-            if value == 1:
+            if value == 1:  # top middle
                 self.game.display.blit(wall_mid, (x * 16, y * 16))
                 self.game.display.blit(wall_top_mid, (x * 16, (y - 1) * 16))
-            # elif value == 2:
-            #     self.game.display.blit(wall_side_front_left, (x * 16, y * 16))
-            # elif value == 3:
-            #     self.game.display.blit(wall_side_front_right, (x * 16, y * 16))
-            elif value == 4:
-                self.game.display.blit(wall_side_mid_left, ((x-0.7) * 16, y * 16))
-            elif value == 5:
-                self.game.display.blit(wall_side_mid_right, ((x+0.7) * 16, y * 16))
-            elif value == 6:
-                self.game.display.blit(wall_inner_corner_mid_left, (x * 16, y * 16))
+            elif value == 2:  # bottom middle
+                self.game.display.blit(wall_mid, (x * 16, (y + (1 - self.render_space)) * 16))
+                self.game.display.blit(wall_top_mid, (x * 16, (y - self.render_space) * 16))
+            elif value == 3:  # left side
+                self.game.display.blit(wall_side_mid_left, (x * 16, y * 16))
+            elif value == 4:  # right side
+                self.game.display.blit(wall_side_mid_right, (x * 16, y * 16))
+            elif value == 5:  # inner top left corner
+                self.game.display.blit(wall_left, (x * 16, y * 16))
                 self.game.display.blit(wall_top_left, (x * 16, (y - 1) * 16))
-            elif value == 7:
-                self.game.display.blit(wall_inner_corner_mid_rigth, (x * 16, y * 16))
+            elif value == 6:  # inner bottom left corner
+                self.game.display.blit(wall_inner_corner_mid_left, (x * 16, (y + (1 - self.render_space)) * 16))
+                self.game.display.blit(wall_corner_top_left, (x * 16, (y - self.render_space) * 16))
+            elif value == 7:  # outer top left corner
+                self.game.display.blit(wall_side_mid_left, (x * 16, y * 16))
+                self.game.display.blit(wall_side_top_left, (x * 16, (y - 1) * 16))
+            elif value == 8:  # outer bottom left corner
+                self.game.display.blit(wall_side_front_left, (x * 16, (y + (1 - self.render_space)) * 16))
+                self.game.display.blit(wall_side_top_left, (x * 16, (y - self.render_space) * 16))
+            elif value == 9:  # inner top right corner
+                self.game.display.blit(wall_right, (x * 16, y * 16))
                 self.game.display.blit(wall_top_right, (x * 16, (y - 1) * 16))
+            elif value == 10:  # inner bottom right corner
+                self.game.display.blit(wall_inner_corner_mid_right, (x * 16, (y + (1 - self.render_space)) * 16))
+                self.game.display.blit(wall_corner_top_right, (x * 16, (y - self.render_space) * 16))
+            elif value == 11:  # outer top right corner
+                self.game.display.blit(wall_side_mid_right, (x * 16, y * 16))
+                self.game.display.blit(wall_side_top_right, (x * 16, (y - 1) * 16))
+            elif value == 12:  # outer bottom left corner
+                self.game.display.blit(wall_side_front_right, (x * 16, (y + (1 - self.render_space)) * 16))
+                self.game.display.blit(wall_side_top_right, (x * 16, (y - self.render_space) * 16))
 
+        # draw other objects
         for x, y in self.plant:
             self.game.display.blit(plant, (x * 16, y * 16))
         for x, y in self.chest:
@@ -88,29 +116,40 @@ class Map:
         return pygame.image.load(tile)
 
     def is_wall(self, x, y):
-        test = set()
-        test.add((x, y))
-        return self.wall.issuperset(test)
+        return (x, y) in self.wall
 
     def is_floor(self, x, y):
-        test = set()
-        test.add((x, y))
-        return self.floor.issuperset(test)
+        """ Check if (x, y) is floor or other objects except walls. """
+        return (x, y) in self.floor or (x, y) in self.chest or (x, y) in self.plant
 
     def wall_render(self, x, y):
         tile_number = 0
         isWall = self.is_wall
         isFloor = self.is_floor
 
-        if isWall(x - 1, y) and isWall(x + 1, y):
+        if isWall(x - 1, y) and isWall(x + 1, y) and isFloor(x, y + 1):  # top middle
             tile_number = 1
-        elif isWall(x, y + 1) and isWall(x, y - 1) and isFloor(x + 1, y):
+        elif isWall(x - 1, y) and isWall(x + 1, y):  # bottom middle
+            tile_number = 2
+        elif isWall(x, y + 1) and isWall(x, y - 1) and isFloor(x + 1, y):  # left side
+            tile_number = 3
+        elif isWall(x, y + 1) and isWall(x, y - 1):  # right side
             tile_number = 4
-        elif isWall(x, y + 1) and isWall(x, y - 1) and isFloor(x - 1, y):
+        elif (isWall(x + 1, y) and isWall(x, y - 1)) and isFloor(x - 1, y + 1):  # inner top left corner
             tile_number = 5
-        elif (isWall(x + 1, y) and isWall(x, y + 1)) or (isWall(x + 1, y) and isWall(x, y - 1)):
+        elif (isWall(x + 1, y) and isWall(x, y + 1)) and isFloor(x - 1, y - 1):  # inner bottom left corner
             tile_number = 6
-        elif (isWall(x - 1, y) and isWall(x, y + 1)) or (isWall(x + 1, y) and isWall(x, y - 1)):
+        elif (isWall(x + 1, y) and isWall(x, y + 1)):  # outer top left corner
             tile_number = 7
+        elif (isWall(x + 1, y) and isWall(x, y - 1)):  # outer bottom left corner
+            tile_number = 8
+        elif (isWall(x - 1, y) and isWall(x, y - 1)) and isFloor(x + 1, y + 1):  # inner top right corner
+            tile_number = 9
+        elif (isWall(x - 1, y) and isWall(x, y + 1)) and isFloor(x + 1, y - 1):  # inner bottom left corner
+            tile_number = 10
+        elif (isWall(x - 1, y) and isWall(x, y + 1)):  # outer top right corner
+            tile_number = 11
+        elif (isWall(x - 1, y) and isWall(x, y - 1)):  # outer bottom left corner
+            tile_number = 12
         return tile_number
 
