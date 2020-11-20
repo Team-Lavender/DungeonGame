f4306b37c150faca7f62a11d6ae539cb2adddc9f
James Hendry <hendry103@gmail.com>
2020-11-20 11:26:05 +0000

    fixed damaged floor tiles not rendering

--- map.py
+++ map.py
@@ -116,8 +116,8 @@ class Map:
                 elif patch == '-':
                     self.floor.add((x + self.map_offset[0], y + self.map_offset[1]))
                     tile_num = self.rand_tiles(len(self.floor_tile_tuple))
-                    # self.floor_render.add((x + self.map_offset[0], y + self.map_offset[1], tile_num))
-                    self.floor_render.add((x + self.map_offset[0], y + self.map_offset[1], 0))
+                    self.floor_render.add((x + self.map_offset[0], y + self.map_offset[1], tile_num))
+                    # self.floor_render.add((x + self.map_offset[0], y + self.map_offset[1], 0))
                 elif patch == 'S':
                     self.floor.add((x + self.map_offset[0], y + self.map_offset[1]))
                     self.floor_render.add((x + self.map_offset[0], y + self.map_offset[1], 0))
