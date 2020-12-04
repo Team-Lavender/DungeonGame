74b933e98f927e464e45736902142a019d0fdf3e
WelchGeorge <georgewelch@me.com>
2020-12-04 10:57:28 +0000

    changed level to 1

--- game.py
+++ game.py
@@ -71,7 +71,7 @@ class Game:
         self.inventory_full_error = False
         self.display_text_counter = 20
         self.paused = False
-        self.level = 2
+        self.level = 1
 
 
     def check_events(self):
