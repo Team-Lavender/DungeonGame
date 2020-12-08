ba0377b1e4a381f25c2dd449f3b3795632e9038a
WelchGeorge <georgewelch@me.com>
2020-12-08 17:29:24 +0000

    added build setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+import cx_Freeze
+
+executables = [cx_Freeze.Executable("main.py", base = "Win32GUI")]
+
+buildOptions = dict(packages = ["pygame", "configparser", "random"], include_files = ['assets/', 'game_saves/', 'mapframe.txt', 'mapframe2.txt', 'mapframe3.txt'])
+
+cx_Freeze.setup(
+         name = "DungeonGame",
+         version = "0.1",
+         options = dict(build_exe = buildOptions),
+         executables = executables)
