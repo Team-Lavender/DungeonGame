4782af7bc247af0b8303dcab85214960dfab192e
WelchGeorge <georgewelch@me.com>
2020-12-08 18:46:47 +0000

    added tutorial mapframe to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import cx_Freeze
 
 executables = [cx_Freeze.Executable("main.pyw", base = "Win32GUI")]
 
-buildOptions = dict(packages = ["pygame", "configparser", "random"], include_files = ['assets/', 'game_saves/', 'mapframe.txt', 'mapframe2.txt', 'mapframe3.txt'])
+buildOptions = dict(packages = ["pygame", "configparser", "random"], include_files = ['assets/', 'game_saves/', 'mapframe.txt', 'mapframe2.txt', 'mapframe3.txt', 'mapframe_tutorial.txt'])
 
 cx_Freeze.setup(
          name = "DungeonGame",
