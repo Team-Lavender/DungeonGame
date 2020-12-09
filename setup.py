388e11a3f407fd2f8976458a2120de4c98c4da3c
Marios Pastos <unconfigured@null.spigotmc.org>
2020-12-09 17:17:04 +0000

    Cutscene fixes

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 import cx_Freeze
 import sys
+import os
+
+
 base_setup = 'Win32GUI' if sys.platform == 'win32' else None
 executables = [cx_Freeze.Executable("main.pyw", base = base_setup, icon='dungeongameicon.ico', targetName='CavernousDepths.exe')]
 
-buildOptions = dict(packages = ["pygame", "configparser", "random"], include_files = ['assets/', 'game_saves/', 'dungeongameicon.png', 'mapframe.txt', 'mapframe2.txt', 'mapframe3.txt', 'mapframe_tutorial.txt'])
+buildOptions = dict(packages = ["pygame", "configparser", "random"], include_files = [os.path.abspath("assets/'"),os.path.abspath("game_saves/"),os.path.abspath("dungeongameicon.png"),os.path.abspath("mapframe.txt"),os.path.abspath('mapframe2.txt'),os.path.abspath("mapframe3.txt") ,os.path.abspath("mapframe_tutorial.txt")])
 
 cx_Freeze.setup(
          name = "DungeonGame",
