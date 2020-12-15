2a488f016788e3d48eea90321edace75b3089bc7
MoonClinical <boy.mehdi@live.fr>
2020-12-15 20:52:16 +0000

    Updated boss health

--- boss_lookup.py
+++ boss_lookup.py
@@ -1,7 +1,7 @@
 # each boss has a list of attributes:
 # [health, shield, level, move_speed, combat_style, ai_type, vision_radius, attack_radius, attack_damage, cooldown, drops{item_name: drop chance}]
-bosses = {"boss": {"big_wizard": [3500, 0, 20, 1, "melee", "patrol", 500, 10, 15, 1500, {"coins": 70}],
+bosses = {"boss": {"big_wizard": [4000, 0, 20, 1, "melee", "patrol", 500, 10, 15, 1500, {"coins": 70}],
 
-                    "super_mage": [5000, 0, 30, 1, "melee", "patrol", 1000, 15, 15, 1000, {"coins": 100}],
+                    "super_mage": [10000, 0, 30, 1, "melee", "patrol", 1000, 15, 15, 1000, {"coins": 100}],
 
-                   "greenhead": [2000, 0, 10, 1, "melee", "patrol", 1000, 10, 15, 1000, {"coins": 150}]}}
+                   "greenhead": [7000, 0, 10, 1, "melee", "patrol", 1000, 10, 15, 1000, {"coins": 150}]}}
